fa4e84112d4ae5c0bd4b0c9cd709d4b5691c012d
impacta-cca <78861105+impacta-cca@users.noreply.github.com>
2021-03-11 15:29:25 -0300

    att ex 08

---
+++ AC-02/ex_08/source/EmptyQueueException.java
@@ -0,0 +1,13 @@
+package source;
+
+@SuppressWarnings("serial")
+
+public class EmptyQueueException extends RuntimeException {
+
+	public EmptyQueueException(String err) {
+
+		super(err);
+
+	}
+
+}
---
+++ AC-02/ex_08/source/FullQueueException.java
@@ -0,0 +1,13 @@
+package source;
+
+@SuppressWarnings("serial")
+
+public class FullQueueException extends RuntimeException {
+
+	public FullQueueException(String err) {
+
+		super(err);
+
+	}
+
+}
